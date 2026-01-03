a5fab1ee54be315ce65a2395ee05300873d823b0
MichaelNotych <notychweb@gmail.com>
2026-01-03 23:50:44 +0700

    feat: updated backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
+        target: 'https://costnotes-api.notych.dev',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
