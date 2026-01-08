af85b6b91a632472f7d7c5be69137103938d210b
MichaelNotych <notychweb@gmail.com>
2026-01-08 23:47:05 +0700

    feat: added pwa config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,32 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'CostNotes',
+        short_name: 'CN',
+        description: 'Minimalistic expense tracker powered with AI',
+        theme_color: '#27272a',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+  })],
   server: {
     port: 3000,
     host: true,
